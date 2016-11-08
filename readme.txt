cc4855ee79035c74204b012b96ed7454485e3b6f
miracl2l22 <miracl2l22@gmail.com>
2016-11-08 21:35:29 +0900

    Update

---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Plugin Name ===
+Contributors: miracl2l22
+Donate link: http://
+Tags: korea, korean, hangeul, font
+Requires at least: 4.0
+Tested up to: 4.6.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+미생체
+
+== Description ==
+
+윤태호 작가님의 손글씨로 만든 미생체
+
+미생체 다운 (.ttf): http://webtoon.daum.net/event/misaengfont
+
+== Installation ==
+
+1. `/wp-content/plugins/sdmisaeng-font`에 플러그인을 업로드 합니다.
+2. `sdmisaeng-font` 플러그인(plugin)을 환성화(activate) 합니다.
+
+== Frequently Asked Questions ==
+
+= Q =
+
+A
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.1 =
+미생체 스타일시트(stylesheet) 적용
+
+== Upgrade Notice ==
+
+== Arbitrary section ==
+
+== A brief Markdown Example ==
+
+`<?php code(); // goes in backticks ?>`
--- sdmisaeng-font.php
+++ sdmisaeng-font.php
@@ -1,10 +1,27 @@
 <?php
 /**
- * Plugin Name: SDMiSaeng font
- * Description: 미생체
+ * Plugin Name: 한글 폰트 - Hangeul Font
+ * Description: 워드프레스용 한글 폰트 - Hangeul Font for WordPress
  * Version: 0.1
  * Author: miracl2l22
+ * License: GPL2
  */
+
+/*  Copyright 2016  miracl2l22  (email : miracl2l22@gmail.com)
+
+    Hangeul Font is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    Hangeul Font is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hangeul Font; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 // Exit if accessed directely
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -16,10 +33,10 @@ if ( ! class_exists( 'SDMiSaeng_font' ) ) {
 		private static $instance;
 
 		public function __construct() {
-		  add_action( 'wp_enqueue_scripts', array( $this, 'register_font_style' ) );
+		  add_action( 'wp_enqueue_scripts', array( $this, 'register_font_css' ) );
 		}
 
-		public function register_font_style() {
+		public function register_font_css() {
 			wp_register_style('sdmisaeng-font', plugins_url( 'sdmisaeng-font/font/SDMiSaeng.css' ) );
 			wp_enqueue_style('sdmisaeng-font');
 		}
